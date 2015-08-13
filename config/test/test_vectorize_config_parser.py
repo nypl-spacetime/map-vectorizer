80df135e0f9e4a5953fd64792c279a6fc2d71bd7
Thomas Levine <_@thomaslevine.com>
2015-08-13 11:04:35 -0400

    add the expectation

--- config/test/test_vectorize_config_parser.py
+++ config/test/test_vectorize_config_parser.py
@@ -5,6 +5,23 @@ from ..vectorize_config_parser import parse
 import pytest
 
 def test_parse():
+    expected = {
+        'basecolors': [
+             [205, 200, 186],
+             [194, 175, 170],
+             [191, 172, 167],
+             [185, 172, 163],
+             [180, 185, 163],
+             [178, 179, 157],
+             [207, 202, 182],
+             [197, 193, 157],
+             [178, 181, 172]
+        ],
+        'brightness': -50,
+        'contrast': 95,
+        'thresholdblack': 160,
+        'thresholdwhite': 255,
+    }
     with open(os.path.abspath(os.path.join(__file__, '..', '..', 'vectorize_config_default.txt'))) as fp:
         observed = parse(fp)
-        assert observed == {}
+        assert observed == expected
