ea554494088c2a7a9bc222123a5015fb4238eb65
Thomas Levine <_@thomaslevine.com>
2015-08-13 11:03:18 -0400

    use the right filename

--- config/test/test_vectorize_config_parser.py
+++ config/test/test_vectorize_config_parser.py
@@ -1,8 +1,10 @@
+import os
+
 from ..vectorize_config_parser import parse
 
 import pytest
 
 def test_parse():
-    with open(os.path.join(__file__, '..', 'vectorize_config_default.txt')) as fp:
+    with open(os.path.abspath(os.path.join(__file__, '..', '..', 'vectorize_config_default.txt'))) as fp:
         observed = parse(fp)
         assert observed == {}
