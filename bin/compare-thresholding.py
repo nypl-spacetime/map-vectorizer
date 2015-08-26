9488be6005978195e4dc2aac020bf3c6d61d3d75
Thomas Levine <_@thomaslevine.com>
2015-08-26 10:02:33 -0400

    write to several files

--- bin/compare-thresholding.py
+++ bin/compare-thresholding.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os, logging, string, subprocess, re
+from itertools import product
 
 def gimp(inputfile:'file', thresholdfile:'file',
          brightness, contrast, thresholdblack, thresholdwhite,
@@ -9,14 +10,13 @@ def gimp(inputfile:'file', thresholdfile:'file',
     thresholdstring = '(gimp-threshold drawable ' + str(thresholdblack) + ' ' + str(thresholdwhite) + ')'
     gimpcommand = '(let* ((image (car (file-tiff-load RUN-NONINTERACTIVE "' + inputfile + '" "' + inputfile + '"))) (drawable (car (gimp-image-get-layer-by-name image "Background")))) (gimp-selection-none image) ' + contraststring + ' ' + thresholdstring + ' (gimp-file-save RUN-NONINTERACTIVE image drawable "' + thresholdfile + '" "' + thresholdfile + '") (gimp-image-delete image))'
 
-    if (not os.path.isfile(thresholdfile)):
-        command = gimp_path + ' -i -b \'' + gimpcommand + '\' -b \'(gimp-quit 0)\''
-        logging.debug(command)
-        os.system(command)
+    command = gimp_path + ' -i -b \'' + gimpcommand + '\' -b \'(gimp-quit 0)\''
+    os.system(command)
 
 brightness = range(-127, 128, 16)
 contrast = range(0, 100, 10)
 thresholdblack = range(0, 255, 16)
 thresholdwhite = range(0, 255, 16)
 
-gimp('test.tif', '/tmp/threshold.tif', -50, 95, 160, 255)
+for args in product(brightness, contrast, thresholdblack, thresholdwhite):
+    gimp('test.tif', '/tmp/gimp_%d_%d_%d_%d.tif' % args, *args)
